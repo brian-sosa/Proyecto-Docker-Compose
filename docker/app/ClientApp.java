d6e49f2e4bec63e712f9a5e9266133ea285c0c18
brian-sosa <briansosaalias@gmail.com>
2024-11-13 18:36:50 -0300

    ClientApp.java

--- docker/app/ClientApp.java
+++ docker/app/ClientApp.java
@@ -1,1 +1,25 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
+public class ClientApp {
+    public static void main(String[] args) {
+        String url = System.getenv("DB_URL");
+        String user = System.getenv("DB_USER");
+        String password = System.getenv("DB_PASSWORD");
+
+        try (Connection conn = DriverManager.getConnection(url, user, password);
+             Statement stmt = conn.createStatement()) {
+
+            ResultSet rs = stmt.executeQuery("SELECT * FROM example_table");
+            while (rs.next()) {
+                System.out.println("ID: " + rs.getInt("id") + 
+                                   ", Name: " + rs.getString("name") + 
+                                   ", Value: " + rs.getInt("value"));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
